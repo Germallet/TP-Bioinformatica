d989155aeb4fe4a9eea17c9893a996ce67ac3740
Germán <germanmallet@gmail.com>
2022-11-09 20:32:00 -0300

    Ex1 from db

--- Ex1.pm
+++ Ex1.pm
@@ -8,15 +8,15 @@ use Bio::DB::GenBank;
 use Bio::SeqIO;
 
 print "TP Bioinformática - Ejercicio 1\n";
-if ($#ARGV + 1 != 2) {
-    print "Se requieren dos parámetros! El archivo de secuencias de entrada y el archivo FASTA de salida.\n";
+if ($#ARGV + 1 < 2) {
+    print "Se requieren al menos dos parámetros! El archivo de secuencias de entrada y el archivo FASTA de salida.\n";
     exit;
 }
 
-#sub get_seq_from_db {
-#    my $db_obj = Bio::DB::GenBank->new;
-#    return $db_obj->get_Seq_by_acc($_[0]);
-#}
+sub get_seq_from_db {
+    my $db_obj = Bio::DB::GenBank->new;
+    return $db_obj->get_Seq_by_acc($_[0]);
+}
 
 sub get_seq_from_file {
     my $seqio_obj = Bio::SeqIO->new(-file => $_[0], -format => "genbank"); 
@@ -33,6 +33,12 @@ sub export_to_fasta {
     $seqio_obj->write_seq($_[0]->revcom->translate(-frame => 2));
 }
 
-my $seq = get_seq_from_file($ARGV[0]);
+my $seq;
+if ($#ARGV >= 3 && $ARGV[2] == 'db') {
+    $seq = get_seq_from_db($ARGV[0]);
+} else {
+    $seq = get_seq_from_file($ARGV[0]);
+}
+
 export_to_fasta($seq, ">$ARGV[1]");
 print "Ok\n";
